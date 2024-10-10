4cf6a2cd89a32d9f7eef6fae74ee761ed8f8b81d
aydelottetaylor <115733242+aydelottetaylor@users.noreply.github.com>
2024-10-09 19:49:51 -0600

    Started On Server
    Started on the server

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,22 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        // var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        // System.out.println("♕ 240 Chess Server: " + piece);
+        try {
+            var port = 1984;
+            if(args.length >= 1) {
+                port = Integer.parseInt(args[0]);
+            }
+
+            var server = new Server();
+            server.run(port);
+            port = server.port();
+            System.out.println("Server started on port: " + port);
+        } catch (Throwable e) {
+            System.out.println("Unable to start server: " + e.getMessage());
+        }
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,12 +4,25 @@ import spark.*;
 
 public class Server {
 
+    public Server() {
+
+    }
+
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", this::registerUser);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::closeSession);
+        Spark.get("/game", this:: getGame);
+        Spark.post("/game", this::createGame);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", this::deleteDatabase);
+
+        // Spark.exception(ResponseException.class, this::exceptionHandler);
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -18,8 +31,44 @@ public class Server {
         return Spark.port();
     }
 
+    public int port() {
+        return Spark.port();
+    }
+
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    // private void exceptionHandler(ResponseException ex, Request req, Response res) {
+    //     res.status(ex.StatusCode());
+    // }
+
+    private Object registerUser(Request req, Response res) {
+        return "";
+    }
+
+    private Object login(Request req, Response res) {
+        return "";
+    }
+
+    private Object closeSession(Request req, Response res) {
+        return "";
+    }
+
+    private Object getGame(Request req, Response res) {
+        return "";
+    }
+
+    private Object createGame(Request req, Response res) {
+        return "";
+    }
+
+    private Object joinGame(Request req, Response res) {
+        return "";
+    }
+
+    private Object deleteDatabase(Request req, Response res) {
+        return "";
     }
 }
